66f23d6b5f6eef116050a78dae23bb532ef48f56
DEBANJAN DAS MANDAL <49723282+DYNAMIC-DENOM@users.noreply.github.com>
2019-11-26 21:16:48 +0530

    return

---
+++ function3.py
@@ -0,0 +1,18 @@
+def square(n):
+    sqr = n*n
+    # print("The square of", n, "is", sqr)
+    return sqr
+x=square(5)
+print(x)
+print(x+25)
+
+def hey():
+    print("hi")
+hey()
+print(hey())
+
+def hello():
+    return"hellow"
+hello()
+print(hello())
+
---
+++ function3A.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return(a+b)
+x=add(5,5)
+print(x)
+print(add(5,5))
+
+
+def add2(a,b):
+    print(a+b)
+add2(5,6)
+print(add2(5,6))
